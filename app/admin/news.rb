bf5d8c9749d4f62249bc6b8a27adc00937dd03a4
vernicode <konstantinvernigorov.kot03@gmail.com>
2023-06-16 01:40:04 +0300

    add news

--- app/admin/news.rb
+++ app/admin/news.rb
@@ -1,3 +1,20 @@
 ActiveAdmin.register News do
+    permit_params :title, :description
 
+    index do
+        selectable_column
+        id_column
+        column :title
+        column :description
+        column :created_at
+        actions
+    end
+    
+    show do
+        attributes_table do
+          row :title
+          row :description
+          row :created_at
+        end
+    end
 end
